29706c6cfd980945f78f7c9843f981eb29f67b5f
Oleksandr Lidak <Oleksandr Lidak>
2016-03-14 16:43:01 +0200

    homePage protractor files is changed.

--- tests/e2e/homePage.js
+++ tests/e2e/homePage.js
@@ -4,6 +4,6 @@ describe('homePage', function() {
   it('Create-new-month-plan link should redirect user to a newPlanPage', function () {
     browser.get('http://localhost:5000/#/');
     element(by.id('new-plan-link')).click();
-    expect(!!browser.getCurrentUrl().match(/\/newMonthPlan$/)).toBe(true);
+    expect(browser.getCurrentUrl()).toMatch(/\/newMonthPlan$/);
   });
 });
