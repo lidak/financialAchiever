1843a4c474c82d45adac13aaca269d34044cc067
Oleksandr Lidak <Oleksandr Lidak>
2016-04-06 11:41:11 +0300

    Port env variable added.

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 var express = require('express'),
     app = express(),
-    server;
+    server,
+    port = process.env.PORT ||5000;
 
 app.use('/bower_components', express.static(__dirname + '/app/bower_components'));
 app.use('/js', express.static(__dirname + '/app/js'));
@@ -14,7 +15,7 @@ app.get('*', function(req, res) {
 
 
 function start (callback) {
-    server = app.listen('5000', function() {
+    server = app.listen(port, function() {
         console.log('Static was served by express.');
         callback();
     });
